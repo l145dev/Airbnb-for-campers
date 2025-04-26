7c64aacfb28591f2edb469ead0ff8fab1e331a50
L145 <147063982+legelff@users.noreply.github.com>
2025-04-27 01:16:02 +0200

    day 21: hostdashboard route, get all properties of host, update property of host

--- backend/routes/hostdashboard.js
+++ backend/routes/hostdashboard.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
 import isAuthenticated from "../middleware/auth.js";
 import isHost from "../utils/isHost.js";
+import axios from "axios";
 
 const router = Router();
 
@@ -134,6 +135,192 @@ router.get("/", isAuthenticated, async (req, res, next) => {
         const resolvedPromise = await Promise.all(promises);
 
         res.status(200).json({ success: true, resolvedPromise })
+    }
+
+    catch (error) {
+        // unexpected error, log error
+        console.log(error);
+        return res.status(500).json({ error: "An error occured, please try again." })
+    }
+})
+
+// get details to modifying property details
+router.get("/modify", isAuthenticated, async (req, res, next) => {
+    const { property_id } = req.query;
+
+    if (!property_id) {
+        return res.status(400).json({ success: false, error: "No property_id provided" });
+    }
+
+    try {
+        // save host id
+        const host_id = parseInt(req.user.user_id);
+
+        // check if host
+        const is_owner = await isHost(host_id, prisma);
+
+        if (!is_owner) {
+            return res.status(401).json({ success: false, error: "You are not registered as a host" });
+        }
+
+        // get property details
+        const property = await prisma.properties.findUnique({
+            where: {
+                property_id: parseInt(property_id)
+            },
+
+            select: {
+                property_id: true,
+                property_name: true,
+                property_description: true,
+                city: true,
+                country: true,
+                property_type: true,
+                check_in_time: true,
+                check_out_time: true,
+                price_per_night: true,
+                capacity: true,
+                note_from_owner: true,
+                longitude: true,
+                latitude: true
+            }
+        })
+
+        if (!property) {
+            return res.status(400).json({ success: false, error: "No property found" });
+        }
+
+        // fix up property details
+        // checkin checkout
+        property.check_in_time = new Date(property.check_in_time).getHours();
+        property.check_out_time = new Date(property.check_out_time).getHours();
+
+        // get road and housenumber -> street address
+        // this opensource api is a blessing 🙏
+        const url = `https://nominatim.openstreetmap.org/reverse.php?lat=${property.latitude}&lon=${property.longitude}&zoom=18&format=jsonv2`
+        const location = await axios.get(url);
+        const houseNumber = location.data.address.house_number;
+        const road = location.data.address.road;
+        const streetAddress = `${houseNumber} ${road}`;
+
+        // get postcode
+        const postcode = location.data.address.postcode;
+
+        const returnObj = {
+            ...property,
+            streetAddress,
+            postcode
+        }
+
+        return res.status(200).json({ success: true, returnObj })
+    }
+
+    catch (error) {
+        // unexpected error, log error
+        console.log(error);
+        return res.status(500).json({ error: "An error occured, please try again." })
+    }
+})
+
+// modify the property
+// get details to modifying property details
+router.patch("/modify", isAuthenticated, async (req, res, next) => {
+    const { property_id, property_name, property_description, street_address, postcode, city, country, property_type, check_in_time_raw, check_out_time_raw, price_per_night, capacity, note_from_owner } = req.body;
+
+    if (!property_id) {
+        return res.status(400).json({ success: false, error: "No property_id provided" });
+    }
+
+    try {
+        // save host id
+        const host_id = parseInt(req.user.user_id);
+
+        // check if host
+        const is_owner = await isHost(host_id, prisma);
+
+        if (!is_owner) {
+            return res.status(401).json({ success: false, error: "You are not registered as a host" });
+        }
+
+        // find exact address to put into long/lat, implicit location validation
+        const cleanedAddress = street_address.replace(" ", "+");
+        const cleanedCity = city.replace(" ", "+");
+        const cleanedCountry = country.replace(" ", "+")
+        const url = `https://nominatim.openstreetmap.org/search.php?street=${cleanedAddress}&city=${cleanedCity}&country=${cleanedCountry}&postalcode=${parseInt(postcode)}&format=jsonv2`;
+        const locations = await axios.get(url);
+
+        if (locations.data.length <= 0) {
+            return res.status(400).json({ success: false, error: "Location not found, maybe it's the street address? Correct street address format: 110 Broadway" });
+        }
+
+        // get first (most accurate location)
+        const location = locations.data[0];
+
+        // get long/lat
+        const longitude = location.lon;
+        const latitude = location.lat;
+
+        // checkin and checkout formatting -> logic accross codebase can be improved a lot for accuracy & consistency, no time to do that for now, this is also the reason why i hate working with dates in code
+        const check_in_time = new Date(Date.UTC(1970, 0, 1, parseInt(check_in_time_raw), 0, 0));
+        const check_out_time = new Date(Date.UTC(1970, 0, 1, parseInt(check_out_time_raw), 0, 0));
+
+        // object with all values to possibly update
+        const updates = {
+            property_name,
+            property_description,
+            longitude,
+            latitude,
+            check_in_time,
+            check_out_time,
+            city,
+            country,
+            property_type,
+            price_per_night,
+            capacity,
+            note_from_owner
+        }
+
+        // get existing property
+        const oldProperty = await prisma.properties.findUnique({
+            where: {
+                property_id: parseInt(property_id)
+            }
+        })
+
+        if (!oldProperty) {
+            return res.status(404).json({ error: "Property not found" });
+        }
+
+        // update property
+        const newProperty = {};
+
+        // iterate through values
+        for (const item in oldProperty) {
+            // ignore prototypes in object (preventing unwanted JS behavior)
+            if (updates.hasOwnProperty(item)) {
+                // check if exists and update needed
+                if (oldProperty[item] !== undefined && oldProperty[item] !== updates[item]) {
+                    newProperty[item] = updates[item];
+                }
+            }
+        }
+
+        // check if nothing is updated
+        if (Object.keys(newProperty).length === 0) {
+            // no db operation needed because nothing was updated
+            return res.status(200).json(oldProperty);
+        }
+
+        // update property with new details
+        const updated = await prisma.properties.update({
+            where: {
+                property_id: parseInt(property_id)
+            },
+
+            data: newProperty,
+        })
+
+        return res.status(200).json({ success: true, updated })
     }
 
     catch (error) {
