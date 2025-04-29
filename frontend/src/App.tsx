5e2097525c8dd521d8ac7054f6d7bc13b7244185
L145 <147063982+legelff@users.noreply.github.com>
2025-04-29 11:22:40 +0200

    day 25: add tanstack and react router, set up basic structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar.tsx';
 
 function App() {
   return (
     <>
+      {/* navbar */}
+      <Navbar />
+
+      {/* Navigation */}
       <h1>Hello world</h1>
     </>
   )
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <>
+            <h2>Airbnb Camping</h2>
+        </>
+    )
+}
+
+export default Navbar
