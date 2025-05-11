0f7f05e4604d40c981c3aa3ee6a9df6e5504ece8
L145 <147063982+legelff@users.noreply.github.com>
2025-05-11 17:52:59 +0200

    day 33: finish creating 404 page

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,26 @@
 import './NotFound.css';
+import { Button } from '@/components/ui/button';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
     return (
         <>
-            <h1>NotFound</h1>
+            <div className='notfound'>
+                <h1>404 | Page not found</h1>
+                <div className='controls'>
+                    <Button variant={"default"} onClick={() => navigate(-1)} className='bg-[#3D8B40] hover:bg-[#357A38]'>
+                        Back
+                    </Button>
+
+                    <Button variant={"outline"} asChild>
+                        <Link to={"/home"}>
+                            Home
+                        </Link>
+                    </Button>
+                </div>
+            </div>
         </>
     )
 }
