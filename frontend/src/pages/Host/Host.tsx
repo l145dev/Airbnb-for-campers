f00d6a44abe0d6d31bb12ea8933596f4014fa3ef
L145 <147063982+legelff@users.noreply.github.com>
2025-06-03 23:01:43 +0200

    day 45: host start

--- frontend/src/pages/Host/Host.tsx
+++ frontend/src/pages/Host/Host.tsx
@@ -3,7 +3,16 @@ import './Host.css';
 const Host = () => {
     return (
         <>
-            <h1>Host</h1>
+            {/* SPA */}
+            <div className='host'>
+                <div className='host-container flex flex-row items-center justify-center h-full w-full bg-gray-100'>
+                    <div className='info-container flex flex-col items-center'>
+                        <h1>
+                            Your camp could make $827 on Airbnb camping
+                        </h1>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
