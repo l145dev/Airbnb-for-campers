1a69b9c676a8121644f170b294a5a5747013297a
L145 <147063982+legelff@users.noreply.github.com>
2025-06-11 13:48:07 +0200

    remove domain attribute in cookie

--- backend/app.js
+++ backend/app.js
@@ -63,7 +63,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24, // 24 hours
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // conditionally secure, insecure in development (for speed) -> process.env.NODE_ENV === 'production'
-    domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.l145.be', // in development -> localhost, in production -> .l145.be
+    // domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.l145.be', // in development -> localhost, in production -> .l145.be
     path: '/', // try to change to /airbnb-camping/
     sameSite: 'none'
   },
