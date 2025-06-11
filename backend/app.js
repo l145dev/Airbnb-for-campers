42bb76363b9159349135c2fa53a08d4641a6f8e5
L145 <147063982+legelff@users.noreply.github.com>
2025-06-11 13:56:47 +0200

    revert path in cookie

--- backend/app.js
+++ backend/app.js
@@ -64,7 +64,7 @@ app.use(session({
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // conditionally secure, insecure in development (for speed) -> process.env.NODE_ENV === 'production'
     // domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.l145.be', // in development -> localhost, in production -> .l145.be
-    path: '/airbnb-camping', // try to change to /airbnb-camping/
+    path: '/', // try to change to /airbnb-camping/
     sameSite: 'none'
   },
   rolling: true, // when user makes request to backend, maxAge resets (keeps user logged in if active in the past 24 hours, for UX)
